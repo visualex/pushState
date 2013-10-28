b81e70f1a7d0fa7af0361be0d5f22e2899d2287a
Aleksandr Bogdanov <alex@tresorg.com>
2013-10-28 11:30:29 +0100

    Update pushState.js
    Tested the code. 
    Works properly in Chrome

--- pushState.js
+++ pushState.js
@@ -8,47 +8,82 @@
     This plugin uses the new method avalible in most modern browsers to push history
     and browse using AJAX without having to use #hashtags in your urls.
 
-    Usage
+    Usage $('a').pushState({container:'#content'});
     
-*/
+    */
 
-(function( $ ){
-  $.fn.pushState = function( options ) {  
-    var settings = {};
-
-    //detect if we have possibilities to use window.history.pushState with this browser
-    if( window.history.pushState != undefined ){ 
-        setting.use = false;
-    }
+    (function( $ ){
+      $.fn.pushState = function( options ) {  
+        var settings = {};
 
     //settings
     settings = $.extend( {  
-       container        :   'body',
-       respect_onclick  :    true,
-       event            :   'click',
-       callback         :    function(){},
-       use_title        :    true 
+     container        :   'body',
+     respect_onclick  :    true,
+     event            :   'click',
+     callbefore       :    function(){},
+     callback         :    function(){},
+     use_title        :    true, 
+     use              :    true 
 
-    }, options);
-
-    if( settings.use ){ 
-        return this.each(function() {        
-            $(this).on(settings.event, function(){
-                    url = $(this).attr('href');
-                    $(settings.container).load(url, function(response, status, xhr) {
-                        
-                        title = '';
-                        //we have to do this cause it currently gets ignored in all browsers. 
-                        //One day it will be implemented
-                        if( settings.use_title ){ 
-                            document.title = $(this).attr('title');
-                            title = document.title;
-                        }
-                        window.history.pushState(response, title, url);
-                    });
-            });  
-        });
+ }, options);
+    
+    //detect if we have possibilities to use window.history.pushState with this browser
+    if( typeof window.history.pushState != 'function' ){ 
+        settings.use = false;
     }
 
-  };
+    //watch for the back browser button
+    if( settings.use ){ 
+
+        window.onpopstate = function(e) {
+            if (e.state != null) {
+                $(settings.container).html(e.state);
+                document.title = e.state.pageTitle;
+            }
+        };
+        
+        //push the first state
+        window.history.pushState( $(settings.container).html(), document.title, document.location.href );
+
+        //main
+        $('body').on(settings.event, this.selector, function(event){
+
+                console.log(event); 
+
+                target = event.target;
+
+                url = $(target).attr('href');
+
+                //typical urls that need no action
+                if( url == '#' ){ 
+                    return;
+                }
+                //your funky animations n all
+                settings.callbefore.call();
+
+                $(settings.container).load(url, function(response, status, xhr) {
+                    link_title = '';
+                    //we have to do this cause it currently gets ignored in all browsers. 
+                    //One day it will be implemented
+                    if( settings.use_title ){ 
+                        link_title = $(target).attr('title');
+                        if( link_title != undefined ){ 
+                            document.title = link_title;
+                        }
+                    }
+                    window.history.pushState(response, link_title, url);
+                    //more funky animations 4u
+                    settings.callback.call();
+
+                });
+                //prevent default document.location change
+                event.preventDefault();
+                //makes the plugin chainable
+                return $(target);
+            });
+    }
+
+};
+
 })( jQuery );
